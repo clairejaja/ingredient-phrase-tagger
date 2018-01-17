ee631f7689e5133b9c2794b0d6bcae925048545a
Claire Jaja <claire.jaja@gmail.com>
2018-01-17 13:39:43 -0800

    Modify subprocess call to avoid too many open files

--- ingredient_phrase_tagger/ingredient_parser.py
+++ ingredient_phrase_tagger/ingredient_parser.py
@@ -12,14 +12,14 @@ class IngredientParser:
         with open(ingredient_file, "r") as infile, open(tmp_file, "w") as outfile:
             outfile.write(utils.export_data(infile.readlines()))
         # Run the CRF model on the temp file input
-        crf_result = subprocess.run(["crf_test", "-v", "1", "-m", "{}".format(self.model_path), "{}".format(tmp_file)], stdout=subprocess.PIPE, universal_newlines=True, check=True)
+        result_lines = [""]
+        with subprocess.Popen(["crf_test", "-v", "1", "-m", "{}".format(self.model_path), "{}".format(tmp_file)], stdout=subprocess.PIPE, universal_newlines=True) as crf_result:
+            # Let's take the result and transform it
+            for char in crf_result.stdout.read():
+                result_lines[-1] += char
+                if char == "\n":
+                    result_lines.append("")
         # Remove the temp file
         subprocess.run(["rm", tmp_file], check=True)
-        # No error, so let's take the result and transform it
-        result_lines = [""]
-        for char in crf_result.stdout:
-            result_lines[-1] += char
-            if char == "\n":
-                result_lines.append("")
         return utils.import_data(result_lines)
 
